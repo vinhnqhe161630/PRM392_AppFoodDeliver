3b5eb7da7f70d30328b65428630abd1343c85e9f
chinhminh <chinhminh2003@gmail.com>
2024-10-22 01:48:35 +0700

    model migration

---
+++ FoodDeliver_API/FoodDeliver_API/Models/OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿namespace FoodDeliver_API.Models
+{
+    public class OrderDetail
+    {
+        public Guid Id { get; set; }
+
+        // Foreign Keys
+        public Guid OrderID { get; set; }
+        public Guid FoodID { get; set; }
+
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Total { get; set; }
+
+        // Navigation properties
+        public Order Order { get; set; }
+        public Food Food { get; set; }
+    }
+
+}
