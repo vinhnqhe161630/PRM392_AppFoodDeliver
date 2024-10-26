c6b92d8b9639ae04f65ff751c500bb883ccc96b7
LAPTOP-EKG8C6KJ\maivu <maivubao08012002@gmail.com>
2024-10-26 12:49:13 +0700

    .

--- FoodDeliver_App/app/build.gradle.kts
+++ FoodDeliver_App/app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0") // Use the latest version
     implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Use the latest
-
+    implementation ("com.facebook.shimmer:shimmer:0.5.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
--- FoodDeliver_App/app/src/main/java/com/example/fooddelivery_app/view/MainActivity.java
+++ FoodDeliver_App/app/src/main/java/com/example/fooddelivery_app/view/MainActivity.java
@@ -24,7 +24,10 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
 }
