66c272c3f3a9665ff4a6c7d2107addbb9f93a11f
Nguyen Quang Vinh <VinhNQ45.fpt.com>
2024-10-29 10:01:34 +0700

    .

---
+++ FoodDeliver_API/FoodDeliver_API/ViewModel/Order/AddOrder.cs
@@ -0,0 +1,15 @@
+﻿namespace FoodDeliver_API.ViewModel.Order
+{
+    public class AddOrder
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ShopId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerAddress { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+    }
+}
