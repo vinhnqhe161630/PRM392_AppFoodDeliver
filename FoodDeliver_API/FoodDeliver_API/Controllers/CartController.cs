eca8147876657388d67f78840abdf7ae214847fa
15-Vũ Đình Khiêm <khiemvdhe170306@fpt.edu.vn>
2024-10-29 13:22:32 +0700

    c

---
+++ FoodDeliver_API/FoodDeliver_API/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using FoodDeliver_API.Entities;
+using FoodDeliver_API.Services;
+using FoodDeliver_API.ViewModel.Cart;
+using FoodDeliver_API.ViewModel.Order;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliver_API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CartController : Controller
+	{
+		private readonly IMapper _mapper;
+		private readonly OrderService _orderService;
+		public CartController(IMapper mapper, OrderService orderService)
+		{
+			_mapper = mapper;
+			_orderService = orderService;
+		}
+		[HttpPost]
+		public async Task<IActionResult> createCart(AddCart addcart)
+		{
+			var order = _mapper.Map<Order>(addcart);
+
+
+			return Ok("Add ok");
+		}
+		[HttpDelete]
+		public async Task<IActionResult> deleteCart(AddOrder addorder)
+		{
+			var order = _mapper.Map<Order>(addorder);
+
+
+			return Ok("Add ok");
+		}
+	}
+}
--- FoodDeliver_API/FoodDeliver_API/Helpers/MappingProfile.cs
+++ FoodDeliver_API/FoodDeliver_API/Helpers/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using FoodDeliver_API.Models;
 using FoodDeliver_API.ViewModel.Auth;
+using FoodDeliver_API.ViewModel.Cart;
 using FoodDeliver_API.ViewModel.Order;
 
 namespace FoodDeliver_API.Helpers
@@ -15,7 +16,9 @@ namespace FoodDeliver_API.Helpers
             CreateMap<SignUpModel, Account>();
 
             CreateMap<AddOrder, Order>();
-            CreateMap<AddOrderDetails, OrderDetail>();
+			CreateMap<AddCart, Cart>();
+
+			CreateMap<AddOrderDetails, OrderDetail>();
         }
     }
     
