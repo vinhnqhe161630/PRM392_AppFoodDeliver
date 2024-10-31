0d2593ac07ff64b3ba2856b1ed815790f1667e3d
LAPTOP-EKG8C6KJ\maivu <maivubao08012002@gmail.com>
2024-10-31 11:27:58 +0700

    ui

--- FoodDeliver_App/app/build.gradle.kts
+++ FoodDeliver_App/app/build.gradle.kts
@@ -37,6 +37,9 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Use the latest
     implementation ("com.facebook.shimmer:shimmer:0.5.0")
     implementation ("com.google.android.material:material:1.7.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
